80b3afcd925676cb15b5d155d588ae91ae85889a
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-02 14:50:36 +0900

    add : backward

--- basics.py
+++ basics.py
@@ -4,6 +4,18 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
+        self.creator = None
+
+    def set_creator(self, func):
+        self.creator = func
+
+    def backward(self, func):
+        f = self.creator
+        if f is not None:
+            x = f.input
+            x.grad = f.backward(self.grad)
+            x.backward()
 
 
 class Function:
@@ -11,9 +23,15 @@ class Function:
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        output.set_creator(self)
+        self.input = input
+        self.output = output
         return output
 
     def forward(self, x):
+        raise NotImplementedError
+
+    def backward(self, gy):
         raise NotImplementedError
 
 
@@ -21,7 +39,14 @@ class Square(Function):
     def forward(self, x):
         return x ** 2
 
+    def backward(self, gy):
+        return gy * 2 * self.input.data
+
 
 class Exp(Function):
     def forward(self, x):
         return np.exp(x)
+
+    def backward(self, gy):
+        x = self.input.data
+        return gy * np.exp(x)
--- test_variable.py
+++ test_variable.py
@@ -36,7 +36,36 @@ def testconcat():
     print(y.data)
 
 
+def testbackward():
+    data = np.array([1, 2])
+    x = Variable(data)
+    A = Square()
+    B = Exp()
+    a = A(x)
+    y = B(a)
+    y.grad = np.array([1, 2])
+    a.grad = B.backward(y.grad)
+    x.grad = A.backward(a.grad)
+
+    print(x.grad)
+
+
+def testconttection():
+    data = np.array([1, 2])
+    x = Variable(data)
+    A = Square()
+    B = Exp()
+    a = A(x)
+    y = B(a)
+    assert y.creator == B
+    assert y.creator.input == a
+    assert y.creator.input.creator == A
+    assert y.creator.input.creator.input == x
+
+
 if __name__ == "__main__":
     testSquare()
     testExp()
     testconcat()
+    testbackward()
+    testconttection()

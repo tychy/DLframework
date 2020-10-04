9f720c6e1c6103db84edd574e48a0a3b08948843
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-10-04 12:51:16 +0900

    add : 数値微分

--- basics.py
+++ basics.py
@@ -7,6 +7,14 @@ def as_array(x):
     return x
 
 
+def numerical_diff(f, x, eps=1e-4):
+    x0 = Variable(x.data - eps)
+    x1 = Variable(x.data + eps)
+    y0 = f(x0)
+    y1 = f(x1)
+    return (y1.data - y0.data) / (2 * eps)
+
+
 class Variable:
     def __init__(self, data):
         if data is not None:
